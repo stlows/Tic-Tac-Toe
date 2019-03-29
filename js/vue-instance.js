239ea6521664fefb1928e19f6ab49bf8253f2ac7
Vincent <vincent_beaulieu@live.ca>
2019-03-29 18:40:15 -0400

    color when winner

--- js/vue-instance.js
+++ js/vue-instance.js
@@ -1,11 +1,32 @@
+var defaultStyle = {
+  backgroundColor: "white",
+  color: "black"
+};
+var winningStyle = {
+  backgroundColor: "green",
+  color: "white"
+}
+
 var app = new Vue({
   el: "#app",
   data: {
     symbols: ["X", "O"],
     scores: [0, 0],
-    board: [["", "", ""], ["", "", ""], ["", "", ""]],
+    board: [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ],
+    styles: [
+      [defaultStyle, defaultStyle, defaultStyle],
+      [defaultStyle, defaultStyle, defaultStyle],
+      [defaultStyle, defaultStyle, defaultStyle]
+    ],
+    logs: [],
+    disableAll: false,
     currentPlayerId: 0,
-    difficulty: 1
+    difficulty: 1,
+    timeoutId: null
   },
   methods: {
     currentPlayer() {
@@ -15,12 +36,73 @@ var app = new Vue({
       this.currentPlayerId = (this.currentPlayerId + 1) % 2;
     },
     setTile(x, y) {
+      if (this.disableAll) return;
       if (this.board[x][y] != "") return;
       var newLine = this.board[x];
       newLine.splice(y, 1, this.currentPlayer());
       this.board.splice(x, 1, newLine);
-      alert(this.checkTicTacToe());
-      this.nextPlayer();
+      var ttt = this.checkTicTacToe();
+      //alert(ttt);
+      if (ttt != null) {
+        this.win(ttt);
+      } else {
+        this.nextPlayer();
+      }
+    },
+    win(winner) {
+      this.logs.push(this.currentPlayer() + " wins the round.");
+      this.scores[this.currentPlayerId]++;
+      this.disableAll = true;
+      this.colorWinner(winner);
+      this.timeoutId = setTimeout(this.newRound, 500);
+
+    },
+    setStyle(x, y, style) {
+      var newLine = this.styles[x];
+      newLine.splice(y, 1, style);
+      this.styles.splice(x, 1, newLine);
+    },
+    colorWinner(winner) {
+      if (winner === "h1") {
+        this.setStyle(0, 0, winningStyle);
+        this.setStyle(0, 1, winningStyle);
+        this.setStyle(0, 2, winningStyle);
+      }
+      if (winner === "h2") {
+        this.setStyle(1, 0, winningStyle);
+        this.setStyle(1, 1, winningStyle);
+        this.setStyle(1, 2, winningStyle);
+      }
+      if (winner === "h3") {
+        this.setStyle(2, 0, winningStyle);
+        this.setStyle(2, 1, winningStyle);
+        this.setStyle(2, 2, winningStyle);
+      }
+      if (winner === "v1") {
+        this.setStyle(0, 0, winningStyle);
+        this.setStyle(1, 0, winningStyle);
+        this.setStyle(2, 0, winningStyle);
+      }
+      if (winner === "v2") {
+        this.setStyle(0, 1, winningStyle);
+        this.setStyle(1, 1, winningStyle);
+        this.setStyle(2, 1, winningStyle);
+      }
+      if (winner === "v3") {
+        this.setStyle(0, 2, winningStyle);
+        this.setStyle(1, 2, winningStyle);
+        this.setStyle(2, 2, winningStyle);
+      }
+      if (winner === "d1") {
+        this.setStyle(0, 0, winningStyle);
+        this.setStyle(1, 1, winningStyle);
+        this.setStyle(2, 2, winningStyle);
+      }
+      if (winner === "d2") {
+        this.setStyle(0, 2, winningStyle);
+        this.setStyle(1, 1, winningStyle);
+        this.setStyle(2, 0, winningStyle);
+      }
     },
     clearBoard() {
       for (var i = 0; i < 3; i++) {
@@ -30,9 +112,20 @@ var app = new Vue({
       }
       this.$forceUpdate();
     },
+    resetStyle() {
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          this.styles[i][j] = defaultStyle;
+        }
+      }
+      this.$forceUpdate();
+    },
     newRound() {
+      clearInterval(this.timeoutId);
       this.clearBoard();
+      this.resetStyle();
       this.currentPlayerId = 0;
+      this.disableAll = false;
     },
     count() {
       var count = 0;
